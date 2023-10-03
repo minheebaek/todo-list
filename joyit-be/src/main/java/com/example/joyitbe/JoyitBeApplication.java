b90e301464650ffa8cbd446a672ebae6208f5847
minhee <bsmq100@gmail.com>
2023-10-03 16:07:43 +0900

    FE-BE 분리 파일 업로드

---
+++ joyit-be/src/main/java/com/example/joyitbe/JoyitBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.joyitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyitBeApplication.class, args);
+    }
+
+}
---
+++ joyit-be/src/test/java/com/example/joyitbe/JoyitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joyitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
