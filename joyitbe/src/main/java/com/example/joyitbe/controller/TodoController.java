f246f9968ea98ec982f50fc8d763bdcf3f11d4e3
minhee <bsmq100@gmail.com>
2023-10-14 21:15:09 +0900

    Feat: security-be

--- joyitbe/src/main/java/com/example/joyitbe/controller/TodoController.java
+++ joyitbe/src/main/java/com/example/joyitbe/controller/TodoController.java
@@ -7,6 +7,7 @@ import com.example.joyitbe.service.TodoService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -29,9 +30,8 @@ public class TodoController {
         return ResponseEntity.ok().body(response);
     }
     @PostMapping
-    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+    public ResponseEntity<?> createTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
         try {
-            String temporaryUserId = "temporary-user"; //temporary user id
 
             //1)TodoEntity로 변환
             TodoEntity entity = TodoDTO.todoEntity(dto);
@@ -39,8 +39,8 @@ public class TodoController {
             //2)id를 null로 초기화 생성 당시에는 id가 없어야함
             entity.setId(null);
 
-            //3)임시 유저 아이디를 설정
-            entity.setUserId(temporaryUserId);
+            //3)Authentication Bearer Token을 통해 받은 userId를 넘긴다
+            entity.setUserId(userId);
 
             //4)서비스를 이용해 Todo 엔티티 생성
             List<TodoEntity> entities = service.create(entity);
@@ -63,11 +63,10 @@ public class TodoController {
         }
     }
     @GetMapping
-    public ResponseEntity<?> retrieveTodoList(){ //검색
-        String temporaryUserId="temporary-user"; //temporary user id
+    public ResponseEntity<?> retrieveTodoList(@AuthenticationPrincipal String userId){ //검색
 
         //1)서비스 메서드의 retrieve 메서드를 사용해 todo리스트를 가져옴
-        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+        List<TodoEntity> entities = service.retrieve(userId);
 
         //2)자바 스트림을 통해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환함
         List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -80,14 +79,13 @@ public class TodoController {
     }
 
     @PutMapping
-    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto){
-        String temporaryUserId = "temporary-user"; //temporary user id
+    public ResponseEntity<?> updateTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 
         //1)dto를 entity로 변환
         TodoEntity entity = TodoDTO.todoEntity(dto);
 
         //2)id를 temporaryUserId로 초기화
-        entity.setUserId(temporaryUserId);
+        entity.setUserId(userId);
 
         //3)서비스를 이용해 entity를 업데이트
         List<TodoEntity> entities = service.update(entity);
@@ -103,15 +101,14 @@ public class TodoController {
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto){
+    public ResponseEntity<?> deleteTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
         try {
-            String temporaryUserId = "temporary-user";
 
             //1)TodoEntity로 변환
             TodoEntity entity = TodoDTO.todoEntity(dto);
 
             //2)임시 유저 아이디
-            entity.setUserId(temporaryUserId);
+            entity.setUserId(userId);
 
             //3)entity 삭제
             List<TodoEntity> entities = service.delete(entity);
---
+++ joyitbe/src/main/java/com/example/joyitbe/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package com.example.joyitbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints={@UniqueConstraint(columnNames = "username")})
+public class UserEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid",strategy = "uuid")
+    private String id; //유저에게 고유하게 부여되는 id
+
+    @Column(nullable = false)
+    private String username; //아이디로 사용할 유저네임
+
+    private String password;
+
+    private String role; //사용자 롤, 예:어드민, 일반 사용자
+
+    private String authProvider; //이후 OAuth에서 사용할 유저정보 제공자 : github
+
+
+
+}
